265bcd6c61b3570ee74482b6f31d5730cb6c2553
jm <106303671+jinnieusLab@users.noreply.github.com>
2025-08-19 21:48:00 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 580 ms, Memory: 51076 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] numbers;
+    static int[] accumulated;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer lineFirst = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(lineFirst.nextToken());
+        int M = Integer.parseInt(lineFirst.nextToken());
+
+        numbers = new int[N+1];
+        accumulated = new int[N+1];
+
+        StringTokenizer numbersInput = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            numbers[i] = Integer.parseInt(numbersInput.nextToken());
+        }
+
+        StringBuilder result = new StringBuilder();
+        accumulate(N);
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer indices = new StringTokenizer(br.readLine());
+            int indexI = Integer.parseInt(indices.nextToken());
+            int indexJ = Integer.parseInt(indices.nextToken());
+            result.append(rangeSum(indexI, indexJ)).append("\n");
+        }
+
+        System.out.print(result);
+    }
+
+    // bottom-up
+    static void accumulate(int j) {
+        for (int i = 1; i <= j; i++) {
+            accumulated[i] = numbers[i] + accumulated[i-1];
+        }
+    }
+
+    static int rangeSum(int i, int j) {
+        return accumulated[j] - accumulated[i-1];
+    }
+}
