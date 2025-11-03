4bb2e279bf3fa78478828636d2d57ed329783773
jm <106303671+jinnieusLab@users.noreply.github.com>
2025-11-03 19:55:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,30 +1,25 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    private static int sumPlusGroup(String s) {
+        int sum = 0;
+        for (String t : s.split("\\+")) {
+            if (!t.isEmpty()) sum += Integer.parseInt(t);
+        }
+        return sum;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine().trim();
+        String expr = br.readLine().trim();
 
-        int sum = 0;
-        int num = 0;
-        boolean minus = false; // 한 번이라도 '-'가 등장했는가
+        String[] groups = expr.split("-");
+        int ans = sumPlusGroup(groups[0]); // 첫 그룹은 더하고
+        
+        for (int i = 1; i < groups.length; i++)   // 이후 그룹은 전부 빼기
+            ans -= sumPlusGroup(groups[i]);
 
-        for (int k = 0; k < s.length(); k++) {
-            char c = s.charAt(k);
-
-						// 정수 누적식
-            if (c >= '0' && c <= '9') {
-                num = num * 10 + (c - '0');
-            } else { // '+' or '-'
-                sum += minus ? -num : num; // 지금까지의 숫자 반영
-                num = 0; // 숫자 초기화 (다음 숫자 위해)
-                if (c == '-') minus = true; // 이후 숫자들은 전부 뺀다
-            }
-        }
-
-        // 마지막 숫자 반영
-        sum += minus ? -num : num;
-
-        System.out.println(sum);
+        System.out.println(ans);
     }
 }
